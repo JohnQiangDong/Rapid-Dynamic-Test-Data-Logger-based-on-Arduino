1cbb694795d363af8d04064405744478f31e7ad4
JohnQiangDong <32593035+JohnQiangDong@users.noreply.github.com>
2023-11-12 18:43:11 +0000

    using python to read the data from serial port

--- arduino/LDR_test/LDR_test.ino
+++ arduino/LDR_test/LDR_test.ino
@@ -15,6 +15,6 @@ void loop() {
   frac_part = (LDR_Reading * 100) * (3.3/255) - int_part * 100; //code to calculate the fractions part of a ADC value
   int_part = LDR_Reading * (3.3/255);
   sprintf(buf,"%d.%02d",int_part,frac_part);// and format as a string using sprintf
-  //Serial.println(buf);
-  delay(0.1);
+  Serial.println(buf);
+  delay(1);
 }
--- arduino/LDR_test/LDR_test.py
+++ arduino/LDR_test/LDR_test.py
@@ -1,2 +1,43 @@
 import serial
+import matplotlib.pyplot as plt
+from matplotlib.animation import FuncAnimation
 
+baud_rate = 19200
+port_name = '/dev/tty.usbmodem21201'
+
+ser = serial.Serial(port_name, baud_rate, timeout=1)
+
+count = 0
+LDR_Reading = []
+time = []
+
+for i in range(1,1001):
+    time.append(i)
+
+# Check if the port is open
+if ser.is_open:
+    print(f"Serial port {port_name} is open.")
+else:
+    print(f"Failed to open serial port {port_name}.")
+    exit()
+
+LDR_Reading = []
+time = []
+count = 0
+
+fig, ax = plt.subplots()
+line, = ax.plot(time, LDR_Reading)
+
+def update(frame):
+    global count
+    time.append(count)
+    LDR_Reading.append(float(ser.readline().decode('utf-8').strip()))
+    count += 1
+    line.set_data(time, LDR_Reading)
+    ax.relim()
+    ax.autoscale_view()
+
+ani = FuncAnimation(fig, update, frames=range(1000), interval=1, repeat=False)
+
+plt.show()
+ser.close()
