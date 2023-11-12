31e23cbde2d1aee976d719ff9bb840b75f8737c6
JohnQiangDong <john.qiang21@imperial.ac.uk>
2023-11-12 17:36:49 +0000

    add python file

--- arduino/LDR_test/LDR_test.ino
+++ arduino/LDR_test/LDR_test.ino
@@ -2,7 +2,7 @@
 
 void setup() {
   // put your setup code here, to run once:
-  Serial.begin(9600);
+  Serial.begin(19200);
 }
 
 void loop() {
@@ -14,7 +14,7 @@ void loop() {
   LDR_Reading = (float)analogRead(A0);
   frac_part = (LDR_Reading * 100) * (3.3/255) - int_part * 100; //code to calculate the fractions part of a ADC value
   int_part = LDR_Reading * (3.3/255);
-  sprintf(buf,"%d.%02d V",int_part,frac_part);// and format as a string using sprintf
-  Serial.println(buf);
-  delay(1000);
+  sprintf(buf,"%d.%02d",int_part,frac_part);// and format as a string using sprintf
+  //Serial.println(buf);
+  delay(0.1);
 }
